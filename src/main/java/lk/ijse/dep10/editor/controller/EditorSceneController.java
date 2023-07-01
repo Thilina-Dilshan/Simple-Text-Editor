6fafb6e440b762948c5ccfd801c57b1c774cfbb7
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-07-01 21:13:28 +0530

    Adding find-replace functions

--- src/main/java/lk/ijse/dep10/editor/controller/EditorSceneController.java
+++ src/main/java/lk/ijse/dep10/editor/controller/EditorSceneController.java
@@ -10,12 +10,16 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import lk.ijse.dep10.editor.util.SearchResult;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EditorSceneController {
 
@@ -48,16 +52,22 @@ public class EditorSceneController {
     public FileOutputStream fos;
     public File file;
 
+    public ArrayList<SearchResult> searchResults = new ArrayList<>();
+    public int pos = 0;
+    public boolean caseSensitive = false;
+    public Matcher matcher;
 
     public void initialize() {
+        txtFind.textProperty().addListener((ov, old, current) -> calculateSearchResult());
+        txtEditor.textProperty().addListener((ov, old, current) -> calculateSearchResult());
         txtEditor.textProperty().addListener((value, previous, current) -> {
-            if (current == null) return;
+            if (current==null) return;
             root = (Stage) txtEditor.getScene().getWindow();
             if (!txtEditor.getText().isBlank() || isEdited) {
                 if (!root.getTitle().contains("*")) {//txtEditor.getText().length()==1
                     root.setTitle("*".concat(root.getTitle()));
                     isEdited = true;
-                    System.out.println("isEdited= " + isEdited);
+                    System.out.println("isEdited= "+isEdited);
                 }
             }
         });
@@ -87,7 +97,7 @@ public class EditorSceneController {
             alert.setHeaderText("Text will be lost. Do you want to save changes before closing?");
             Optional<ButtonType> alertResult = alert.showAndWait();
             if (alertResult.isEmpty() || alertResult.get() == ButtonType.CANCEL) return;
-            if (alertResult.get() == ButtonType.NO) {
+            if (alertResult.get() == ButtonType.NO){
                 root.close();
                 System.out.println("root is not null");
             }
@@ -97,7 +107,7 @@ public class EditorSceneController {
             }
 //            }
         }
-        System.out.println("isEdit= " + isEdited + "\tisSaved= " + isSaved);
+        System.out.println("isEdit= "+isEdited +"\tisSaved= "+isSaved);
         root.close();
     }
 
@@ -106,27 +116,27 @@ public class EditorSceneController {
         System.out.println("New text");
         if (isEdited) {
 //            if (!isSaved) {
-            System.out.println("new=" + isEdited);
+            System.out.println("new="+isEdited);
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
                     "",
                     ButtonType.CANCEL, ButtonType.YES, ButtonType.NO);
             alert.setHeaderText("Text will be lost. Do you want to save before open a new one?");
             Optional<ButtonType> alertResult = alert.showAndWait();
-            if (alertResult.isEmpty() || alertResult.get() == ButtonType.CANCEL) return;
-            if (alertResult.get() == ButtonType.YES) mnSave.fire();
-            if (alertResult.get() == ButtonType.NO) {
+            if (alertResult.isEmpty() || alertResult.get()==ButtonType.CANCEL) return;
+            if (alertResult.get()==ButtonType.YES)mnSave.fire();
+            if (alertResult.get()==ButtonType.NO) {
                 txtEditor.clear();
                 root.setTitle("Untitled");
-                isEdited = false;
+                isEdited=false;
             }
 //            }
         }
         if (isSaved) {
             txtEditor.clear();
-            root = (Stage) txtEditor.getScene().getWindow();
+            root=(Stage) txtEditor.getScene().getWindow();
             root.setTitle("Untitled");
-            isEdited = false;
-            isSaved = false;
+            isEdited=false;
+            isSaved=false;
         }
     }
 
@@ -135,7 +145,7 @@ public class EditorSceneController {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open a file");
         File file = fileChooser.showOpenDialog(txtEditor.getScene().getWindow());
-        if (file == null) return;
+        if (file==null) return;
 
         FileInputStream fis = new FileInputStream(file);
         byte[] bytes = fis.readAllBytes();
@@ -175,14 +185,13 @@ public class EditorSceneController {
     public void rootPaneOnDragDropped(DragEvent dragEvent) throws IOException {
         File droppedFile = dragEvent.getDragboard().getFiles().get(0);
 
-
         FileInputStream fis = new FileInputStream(droppedFile);
         byte[] bytes = fis.readAllBytes();
         fis.close();
         System.out.println("Dropped file :" + droppedFile.getName());
         txtEditor.setText(new String(bytes));
         root.setTitle(droppedFile.getName());
-        isEdited = true;
+        isEdited=true;
     }
 
     public void rootPaneOnDragOver(DragEvent dragEvent) {
@@ -208,23 +217,80 @@ public class EditorSceneController {
     }
 
 
-    public void btnReplaceOnAction(ActionEvent event) {
+    private void calculateSearchResult() {
+        String query = txtFind.getText();
+        if (query.isEmpty()) {
+            lblResult.setText("O Result");
+            return;
+        }
 
+        searchResults.clear();
+        pos = 0;
+        txtEditor.deselect();
+
+
+        Pattern pattern;
+        try {
+            if (caseSensitive)pattern = Pattern.compile(query, Pattern.CASE_INSENSITIVE);   //error=> if not complete the regEx pattern
+            else pattern = Pattern.compile(query);
+        } catch (Exception e) {
+            return;
+        }
+
+        matcher = pattern.matcher(txtEditor.getText());
+
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            SearchResult result = new SearchResult(start, end);
+            searchResults.add(result);
+        }
+        lblResult.setText(String.format("%d Results", searchResults.size()));
+        select();
     }
 
-    public void btnReplaceAllOnAction(ActionEvent event) {
-
-    }
-
-    public void chkMatchCaseOnAction(ActionEvent event) {
-
+    private void select() {
+        if (searchResults.isEmpty()) return;
+        SearchResult searchResult = searchResults.get(pos);
+        txtEditor.selectRange(searchResult.getStart(), searchResult.getEnd());
+        lblResult.setText(String.format("%d/%d Results", (pos+1),searchResults.size()));
     }
 
     public void btnDownOnAction(ActionEvent actionEvent) {
-
+        pos++;
+        if (pos==searchResults.size()) {
+            pos = -1;
+            return;
+        }
+        select();
     }
 
     public void btnUpOnAction(ActionEvent actionEvent) {
+        pos--;
+        if (pos ==-1) { //(pos < 0)
+            pos = searchResults.size();
+            return;
+        }
+        select();
+    }
 
+    public void btnReplaceOnAction(ActionEvent event) {
+        String replaceText = txtReplace.getText();
+        if (replaceText.isEmpty() || lblResult.getText().equals("0 Results")) return;
+        txtEditor.replaceSelection(replaceText);
+        calculateSearchResult();
+    }
+
+    public void btnReplaceAllOnAction(ActionEvent event) {
+        String replaceText = txtReplace.getText();
+        if (replaceText.isEmpty() || lblResult.getText().equals("0 Results")) return;
+        txtEditor.setText(matcher.replaceAll(txtReplace.getText()));
+        calculateSearchResult();
+    }
+
+    public void chkMatchCaseOnAction(ActionEvent event) {
+        if (chkMatchCase.isSelected()) caseSensitive = false;
+        else caseSensitive = true;
+        calculateSearchResult();
     }
 }
